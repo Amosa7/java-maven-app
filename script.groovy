34b0a414120d5df8426714e0fd5e59e1fd06682f
Amosa7 <136304747+Amosa7@users.noreply.github.com>
2023-11-01 18:30:37 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,19 @@
 def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
+    echo "building the application Jar..."
+    sh "mvn package"
+}
 
 def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+    echo "building docker image of the appliction docker Image..."
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        sh "docker build -t amosa77/demo-app:jma-2.4 ."
+        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+        sh "docker push amosa77/demo-app:jma-2.4"
     }
-} 
+}
 
 def deployApp() {
-    echo 'deploying the application...'
-} 
+    echo "deploying the application..."
+}
 
 return this
