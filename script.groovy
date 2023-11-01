08501ba1480ca23c9483f9243a8df0d0ba2213b9
Amosa Bashir <Amosabashir7@gmail.com>
2023-11-01 22:43:53 +0100

    calls library function in Jenkins

--- script.groovy
+++ script.groovy
@@ -1,16 +1,3 @@
-def buildJar() {
-    echo "building the application Jar..."
-    sh "mvn package"
-}
-
-def buildImage() {
-    echo "building docker image of the appliction docker Image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-        sh "docker build -t amosa77/demo-app:jma-2.4 ."
-        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh "docker push amosa77/demo-app:jma-2.4"
-    }
-}
 
 def deployApp() {
     echo "deploying the application..."
